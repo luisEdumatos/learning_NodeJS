85016362af7171631da1c1656b0ab09d11d672bc
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-11 08:39:03 -0300

    Using Morgan

--- demo-nodejs/app.js
+++ demo-nodejs/app.js
@@ -1,12 +1,14 @@
 import express from "express"; 
-import livros from "./data/livros.json"
 import livrosRoute from "./routes/livrosRoute.js"
+import morgan from "morgan";
 
 const PORT = 3000;
 const server = express(); 
 
 const criarUrl = (version, path) => `/api/${version}/${path}`;
 const LIVROS_URL = criarUrl('v1', 'livros');
+
+server.use(morgan("tiny")); 
 
 server.use(LIVROS_URL, livrosRoute); 
 
