47d598c04a0c8d94cd64644fd6604ad9a9d0af5f
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-11 14:39:15 -0300

    Finished CRUD Livros

--- demo-nodejs/routes/livrosRoute.js
+++ demo-nodejs/routes/livrosRoute.js
@@ -24,41 +24,63 @@ const router = express.Router();
 let livrosArray = livros; 
 
 router.get("/", (req, res) => {
-    res.json(livros); 
+    livrosModel.find((err, livros) => { 
+        if(err) { 
+            res.send(500).send(err); 
+        }
+        res.json(livros); 
+    });
 });
 
 router.get("/:id", (req, res) => {
-    const livro = livros.find(value => value.id == req.params.id); 
-    if (livro) { 
-        res.json(livro); 
-    } else { 
-        res.send('Livro não encontrado'); 
-    }
-    res.end(); 
+    livrosModel.findById(req.params.id, (err, livro) => {
+        if (livro) { 
+            res.json(livro); 
+        } else { 
+            res.status(404).send(`Livro com id ${req.params.id} não encontrado`); 
+        }
+    }); 
 })
 
 router.post("/", (req, res) => {
-    console.log(req.body);
-    livrosArray.push(req.body); 
-    res.status(200).send("OK");  
-    res.end(); 
+    const id = new mongoose.Types.ObjectId(); 
+    const livroParaSalvar = Object.assign({
+        _id: id
+    }, req.body); 
+
+    const livro = new livrosModel(livroParaSalvar); 
+    livro.save().then((err, livro) => {
+        if (err) { 
+            res.status(500).send(err); 
+        }
+        res.json(livro); 
+    }); 
 });
 
-router.put("/", (req, res) => {
-    console.log('tratando metodo put'); 
-    res.end(); 
+router.put("/:id", (req, res) => {
+    livrosModel.findById(req.params.id, (err, livro) => {
+        if (livro) { 
+            livro.titulo = req.body.titulo; 
+            livro.tipo = req.body.tipo;
+            livro.save().then((err, livro) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.json(livro); 
+            });
+        } else { 
+            res.status(404).send(`Livro com ID ${req.params.id} não encontrado.`);
+        }
+    });
 });
 
-router.delete("/", (req, res) => {
-    console.log('tratando metodo delete'); 
-    res.end(); 
+router.delete("/:id", (req, res) => {
+    livrosModel.findByIdAndDelete(req.params.id, (err, livro) => {
+        if (err) { 
+            res.status(500).send(err); 
+        }
+        res.status(200).send(`Livro com o id ${req.params.id} deletado com sucesso`); 
+    });
 });
-
-router.param("id", (req, res, next, id) => { 
-    if (isNaN(id)) { 
-        next("Não é numero"); 
-    }
-    next(); 
-}); 
 
 export default router; 
